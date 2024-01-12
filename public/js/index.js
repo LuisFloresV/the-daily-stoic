1878e9ce15b9de99b03c836e09341681576c0c18
AbrilRE <abril-re@hotmail.com>
2024-01-11 21:24:02 -0600

    Add styles for website

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,7 @@ const reflection = document.getElementById('reflection-text');
 fetch('/api/reflection')
   .then((response) => response.json())
   .then((data) => { reflection.innerText = JSON.stringify(data.reflection); });
+
+  function refresh(){
+    window.location.reload();
+} 
