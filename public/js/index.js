79df3a84feebb1e3b140163dc4e7e3bfaf006bac
AbrilRE <abril-re@hotmail.com>
2024-01-11 22:13:02 -0600

    Add typing animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,37 @@
 const reflection = document.getElementById('reflection-text');
 
+
 fetch('/api/reflection')
   .then((response) => response.json())
   .then((data) => { reflection.innerText = JSON.stringify(data.reflection); });
 
-  function refresh(){
-    window.location.reload();
-} 
+function refresh() {
+  window.location.reload();
+}
+
+const div = document.querySelector(".reflection-text");
+let text = reflection.innerText;
+
+function typingAnimation(element, text, i = 0) {
+
+  let timer;
+  reflection.addEventListener("click", () => {
+    reflection.innerText = "Click event";
+    clearTimeout(timer);
+  })
+
+  if (i === 0) {
+    element.textContent = "";
+  }
+
+  element.textContent += text[i];
+
+  if (i === text.length - 1) {
+    return;
+  }
+
+  timer = setTimeout(() => typingAnimation(element, text, i + 1), 60);
+}
+
+typingAnimation(div, text);
+
